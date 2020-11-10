45a24b79199d3d7e4b68555146eca81279403784
Benedict Sullivan <talloakgames@gmail.com>
2020-11-09 19:35:28 -0500

    Expanded API

--- player.php
+++ player.php
@@ -1,8 +1,43 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 function getPlayerInventory($id) {
-	$fp = getcwd()."/paisleycache/players/".$id."/inv.csv"
+	if (!file_exists("paisleycache/players/".$id)) {
+		mkdir("paisleycache/players/".$id, 0777, true);
+	}
+	$fp = getcwd()."/paisleycache/players/".$id."/inv.csv";
 	if(!file_exists($fp)) {
 		$f = fopen($fp, "x+");
 		fclose($f);
+		return [];
+	};
+	if (filesize($fp) == 0) {
+		return [];
 	}
+	$f = fopen($fp, "r");
+	$string = fread($f, filesize($fp));
+	fclose($f);
+	return explode(",\n", $string);
 }
+function getPlayerFriends($id) {
+	if (!file_exists("paisleycache/players/".$id)) {
+		mkdir("paisleycache/players/".$id, 0777, true);
+	}
+	$fp = getcwd()."/paisleycache/players/".$id."/fri.csv";
+	if(!file_exists($fp)) {
+		$f = fopen($fp, "x+");
+		fclose($f);
+		return [];
+	};
+	if (filesize($fp) == 0) {
+		return [];
+	}
+	$f = fopen($fp, "r");
+	$string = fread($f, filesize($fp));
+	fclose($f);
+	return explode(",\n", $string);
+}
+
+echo implode(", ", getPlayerFriends(2));
+?>
