3f8bcc836f6ff58dc3e09ac552341dff7d05d0bc
Tall Oak Games <talloakgames@gmail.com>
2020-11-15 16:11:44 -0500

    Finished Player library

--- player.php
+++ player.php
@@ -2,185 +2,115 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+function loopThroughDirectory($directory) {
+	$fileList = glob($directory.'/*');
+	return $fileList;
+}
+function addFileToDirectory($directory, $name) {
+	$fp = $directory."/".$name;
+	if (fopen($fp, "x+") == FALSE) {
+		return false;
+	} else {
+		return true;
+	}
+}
+function removeItemFromDirectory($directory, $name) {
+	return unlink($directory."/".$name);
+}
 function getPlayerInventory($id) {
 	if (!file_exists("paisleycache/players/".$id)) {
 		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$id."/inv.csv";
-	if(!file_exists($fp)) {
-		$f = fopen($fp, "x+");
-		fclose($f);
-		return [];
-	};
-	if (filesize($fp) == 0) {
-		return [];
+	$fp = getcwd()."/paisleycache/players/".$id."/inv";
+	return loopThroughDirectory($fp);
+}
+function addInventoryItem($id, $friend) {
+	if (!file_exists("paisleycache/players/".$id)) {
+		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$f = fopen($fp, "r");
-	$string = fread($f, filesize($fp));
-	fclose($f);
-	return explode(",\n", $string);
+	$fp = getcwd()."/paisleycache/players/".$id."/inv";
+	addFileToDirectory($fp, $friend);
 }
 function removeInventoryItem($id, $what) {
-	$original = getPlayerSentFriendRequests($id);
-	$fp = getcwd()."/paisleycache/players/".$id."/sentreq.csv";
-	fopen($fp, "w+");
-	$removed = false;
-	foreach ($original as $key => $value) {
-		if (("".$value != "".$what) or ($removed)) {
-			fwrite($fp, $value."\n");
-		} else {
-			$removed = true;
-		}
-	}
-	fclose($fp);
+	$fp = getcwd()."/paisleycache/players/".$id."/inv";
+	return removeItemFromDirectory($fp, $what);
 }
 function getPlayerFriends($id) {
 	if (!file_exists("paisleycache/players/".$id)) {
 		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$id."/fri.csv";
-	if(!file_exists($fp)) {
-		$f = fopen($fp, "x+");
-		fclose($f);
-		return [];
-	};
-	if (filesize($fp) == 0) {
-		return [];
-	}
-	$f = fopen($fp, "r");
-	$string = fread($f, filesize($fp));
-	fclose($f);
-	return explode(",\n", $string);
+	$fp = getcwd()."/paisleycache/players/".$id."/fri";
+	return loopThroughDirectory($fp);
 }
 function addFriend($id, $friend) {
 	if (!file_exists("paisleycache/players/".$id)) {
 		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$id."/fri.csv";
-	$f = fopen($fp, "a");
-	fwrite($f, "".$friend."\n");
-	fclose($f);
+	$fp = getcwd()."/paisleycache/players/".$id."/fri";
+	addFileToDirectory($fp, $friend);
 	if (!file_exists("paisleycache/players/".$friend)) {
 		mkdir("paisleycache/players/".$friend, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$friend."/fri.csv";
-	$f = fopen($fp, "a");
-	fwrite($f, "".$id."\n");
-	fclose($f);
+	$fp = getcwd()."/paisleycache/players/".$friend."/fri";
+	addFileToDirectory($fp, $id);
 }
 function removeFriend($id, $who) {
-	$original = getPlayerSentFriendRequests($id);
-	$fp = getcwd()."/paisleycache/players/".$id."/fri.csv";
-	fopen($fp, "w+");
-	foreach ($original as $key => $value) {
-		if ("".$value != "".$who) {
-			fwrite($fp, $value."\n");
-		}
-	}
-	fclose($fp);
+	$fp = getcwd()."/paisleycache/players/".$id."/fri";
+	removeItemFromDirectory($fp, $who);
+	$fp = getcwd()."/paisleycache/players/".$who."/fri";
+	removeItemFromDirectory($fp, $id);
 }
-function getPlayerActiveFriendRequests($id) {
+function getPlayerActiveRequests($id) {
 	if (!file_exists("paisleycache/players/".$id)) {
 		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$id."/activereq.csv";
-	if(!file_exists($fp)) {
-		$f = fopen($fp, "x+");
-		fclose($f);
-		return [];
-	};
-	if (filesize($fp) == 0) {
-		return [];
-	}
-	$f = fopen($fp, "r");
-	$string = fread($f, filesize($fp));
-	fclose($f);
-	return explode(",\n", $string);
+	$fp = getcwd()."/paisleycache/players/".$id."/activereq";
+	return loopThroughDirectory($fp);
 }
 function removeActiveRequest($id, $from) {
-	$original = getPlayerSentFriendRequests($id);
-	$fp = getcwd()."/paisleycache/players/".$id."/activereq.csv";
-	fopen($fp, "w+");
-	foreach ($original as $key => $value) {
-		if ("".$value != "".$from) {
-			fwrite($fp, $value."\n");
-		}
-	}
-	fclose($fp);
+	$fp = getcwd()."/paisleycache/players/".$id."/activereq";
+	return removeItemFromDirectory($fp, $from);
 }
 function getPlayerSentFriendRequests($id) {
 	if (!file_exists("paisleycache/players/".$id)) {
 		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$id."/sentreq.csv";
-	if(!file_exists($fp)) {
-		$f = fopen($fp, "x+");
-		fclose($f);
-		return [];
-	};
-	if (filesize($fp) == 0) {
-		return [];
-	}
-	$f = fopen($fp, "r");
-	$string = fread($f, filesize($fp));
-	fclose($f);
-	return explode(",\n", $string);
+	$fp = getcwd()."/paisleycache/players/".$id."/sentreq";
+	return loopThroughDirectory($fp);
 }
 function addRequest($id, $from) {
 	if (!file_exists("paisleycache/players/".$id)) {
 		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$id."/activereq.csv";
-	$f = fopen($fp, "a");
-	fwrite($f, "".$from."\n");
-	fclose($f);
-	if (!file_exists("paisleycache/players/".$from)) {
-		mkdir("paisleycache/players/".$from, 0770, true);
+	$fp = getcwd()."/paisleycache/players/".$id."/activereq";
+	addFileToDirectory($fp, $from);
+	if (!file_exists("paisleycache/players/".$id)) {
+		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$from."/sentreq.csv";
-	$f = fopen($fp, "a");
-	fwrite($f, "".$id."\n");
-	fclose($f);
-
+	$fp = getcwd()."/paisleycache/players/".$from."/sentreq";
+	addFileToDirectory($fp, $id);
 }
 function removeSentRequest($id, $from) {
-	$original = getPlayerSentFriendRequests($id);
-	$fp = getcwd()."/paisleycache/players/".$id."/sentreq.csv";
-	fopen($fp, "w+");
-	foreach ($original as $key => $value) {
-		if ("".$value != "".$from) {
-			fwrite($fp, $value."\n");
-		}
-	}
-	fclose($fp);
+	$fp = getcwd()."/paisleycache/players/".$id."/sentreq";
+	return removeItemFromDirectory($fp, $from);
 }
 function getPlayerNotifications($id) {
 	if (!file_exists("paisleycache/players/".$id)) {
 		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$id."/notifications.csv";
-	if(!file_exists($fp)) {
-		$f = fopen($fp, "x+");
-		fclose($f);
-		return [];
-	};
-	if (filesize($fp) == 0) {
-		return [];
-	}
-	$f = fopen($fp, "r");
-	$string = fread($f, filesize($fp));
-	fclose($f);
-	return explode(",\n", $string);
+	$fp = getcwd()."/paisleycache/players/".$id."/notifications";
+	return loopThroughDirectory($fp);
 }
-function sendNotification($id, $notification) {
+function sendNotification($id, $message, $rel) {
 	if (!file_exists("paisleycache/players/".$id)) {
 		mkdir("paisleycache/players/".$id, 0770, true);
 	}
-	$fp = getcwd()."/paisleycache/players/".$id."/notifications.csv";
-	$f = fopen($fp, "a");
-	fwrite($f, htmlspecialchars($notification)."\n");
+	$fp = getcwd()."/paisleycache/players/".$id."/notifications";
+	addFileToDirectory($fp, count(getPlayerNotifications($id)));
+	$f = fopen($fp, "w+");
+	fwrite($f, $message."\n".$rel);
 	fclose($f);
-
 }
 function acceptFriendRequest($id, $from) {
 	if (array_search($id, getPlayerFriends($id), FALSE) === false) {
